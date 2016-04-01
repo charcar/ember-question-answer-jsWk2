36f5fef575da330b02eeed10ac1f17ca2534ff73
Charles Carter <charles.h.carter@gmail.com>
2016-04-01 13:55:25 -0700

    add new question backend implementing

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion1() {
+      var params = {
+        question:this.get('question') ? this.get('question') : "",
+        author:this.get('author') ? this.get('author') : "",
+        notes:this.get('notes') ? this.get('notes') : "",
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
+});
